19353e95d23ccd0d702210f7a31b90dbab98c5fd
Brendon Parker <brendonparker@Brendons-MBP.MG8702>
2022-12-17 15:03:24 -0500

    Working API/Deployment

--- InfrastructureAsCode/InfrastructureAsCodeStack.cs
+++ InfrastructureAsCode/InfrastructureAsCodeStack.cs
@@ -1,13 +1,29 @@
 using Amazon.CDK;
 using Constructs;
+using Lambda = Amazon.CDK.AWS.Lambda; 
+namespace InfrastructureAsCode;
 
-namespace InfrastructureAsCode
+public class InfrastructureAsCodeStack : Stack
 {
-    public class InfrastructureAsCodeStack : Stack
+    internal InfrastructureAsCodeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
-        internal InfrastructureAsCodeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        var lambda = new Lambda.Function(this, "Lambda", new Lambda.FunctionProps
         {
-            // The code that defines your stack goes here
-        }
+            Runtime = Lambda.Runtime.DOTNET_6,
+            MemorySize = 1024,
+            Handler = "SampleApi",
+            Code = new Lambda.AssetCode("./LambdaSource/SampleApi")
+        });
+
+        var functionUrl = lambda.AddFunctionUrl(new Lambda.FunctionUrlOptions
+        {
+            AuthType = Lambda.FunctionUrlAuthType.NONE
+        });
+
+        new CfnOutput(this, "FunctionUrl", new CfnOutputProps
+        {
+            ExportName = "SampleApiFunctionUrl",
+            Value = functionUrl.Url
+        });
     }
 }
--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
