33964952f1d63bc01788c123721baa726627a13d
Brendon Parker <brendonparker@Brendons-MBP.MG8702>
2022-12-17 14:34:40 -0500

    Add CDK init

---
+++ InfrastructureAsCode/InfrastructureAsCodeStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace InfrastructureAsCode
+{
+    public class InfrastructureAsCodeStack : Stack
+    {
+        internal InfrastructureAsCodeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
